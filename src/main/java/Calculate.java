6da4bb17b667ec5d58a60b7510a6158d782bf185
A237906 <lincoln.mgijima@standardbank.co.za>
2020-02-18 10:02:21 +0200

    Removed unnecessary files and lines

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -2,34 +2,34 @@ import java.util.ArrayList;
 
 public class Calculate {
     public static int add(String input) {
-        StringBuilder delimeter = new StringBuilder(",\n");
-        if (input.startsWith("//")) {
-            delimeter = new StringBuilder(input.substring(input.indexOf("//") + 2, input.indexOf("\n")));
+        StringBuilder delimiter = new StringBuilder(",\n");
+        if (input.startsWith("//")) { // looking for delimiters with startIndex of 2 and endIndex of breakLine
+            delimiter = new StringBuilder(input.substring(input.indexOf("//") + 2, input.indexOf("\n")));
             input = input.substring(input.indexOf("\n"));
         }
-        delimeter = new StringBuilder("[" + delimeter + "]");
-        return add(input, delimeter.toString());
+        delimiter = new StringBuilder("[" + delimiter + "]");
+        return add(input, delimiter.toString());
     }
 
     static int add(final String numbers, String delimiter) {
         String[] arr = numbers.split("[" + delimiter + "]");
-        ArrayList<Integer> negativeNumbers = new ArrayList<>();
+        ArrayList<Integer> negativeNumbers = new ArrayList<>(); //Tracking negative numbers to be displayed later
         int sum = 0;
         try {
             for (String ans : arr) {
                 if (!ans.trim().isEmpty()) {
-                    int i = Integer.parseInt(ans.trim());
+                    int i = Integer.parseInt(ans.trim()); // converting string to integer
                     if (i < 0) {
                         negativeNumbers.add(i);
-                    } else if (i < 1000) {
+                    } else if (i < 1000) {  //ignores integers greater than or equal to 1000
                         sum += i;
                     }
                 }
-            }
+            } //
         } catch (IllegalArgumentException e) {
             System.out.println(e);
         }
-        if (!negativeNumbers.isEmpty())
+        if (!negativeNumbers.isEmpty()) // throw going to be handled by assertThrows
             throw new IllegalArgumentException("\nERROR: negatives not allowed " + negativeNumbers.toString().replace("[", "").replace("]", ""));
         return sum;
     }
